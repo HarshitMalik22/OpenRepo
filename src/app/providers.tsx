dac228ef486775e4cdf519bdfc96332b758e6525
HarshitMalik22 <harshitmalik29@gmail.com>
2026-01-17 15:25:29 +0530

    Applied system theme as default

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -18,7 +18,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         },
       }}
     >
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem={false} disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
         {children}
       </ThemeProvider>
     </ClerkProvider>
